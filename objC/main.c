b391027b322ae4298d25a1bc750d3f512f79f6b2
LincolnFaradey <iphoenix179@gmail.com>
2014-03-16 11:40:21 +0400

    Initial Commit

---
+++ objC/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  objC
+//
+//  Created by Andrei Nechaev on 3/16/14.
+//  Copyright (c) 2014 Andrei Nechaev. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
